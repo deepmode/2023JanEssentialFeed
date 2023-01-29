d1a5dad0b71ff0ed6fa331600139f316316ab4a6
Eric Ho <puiomode@gmail.com>
2023-01-29 21:21:05 +0800

    Added notes to the HTTPClient protocol

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -23,6 +23,8 @@ class RemoteFeedLoader {
     }
 }
 
+//note: The <HTTPClient> does not need to be a class. It is just a contract defining which external functionality the RemoteFeedLoader needs, so a protocol is a more suitable way to define it.
+//note: By creating a clean separation with protocols, we made the RemoteFeedLoader more flexible, open for extension and more testable.
 protocol HTTPClient {
     func get(from url:URL)
 }
