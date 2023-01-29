823ef46b2134a43477996ca5e7e0bba39b772ece
Eric Ho <puiomode@gmail.com>
2023-01-29 19:23:50 +0800

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Eric Ho on 29/1/2023.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id:UUID
+    let description:String?
+    let location:String?
+    let imageURL:URL
+}
