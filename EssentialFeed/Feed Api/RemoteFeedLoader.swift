6e1769a45177e652429d9f2d6d58a663da0d5274
Eric Ho <puiomode@gmail.com>
2023-02-03 08:21:06 +0800

    Delivery connectivity error on client error

--- EssentialFeed/Feed Api/RemoteFeedLoader.swift
+++ EssentialFeed/Feed Api/RemoteFeedLoader.swift
@@ -10,7 +10,7 @@ import Foundation
 //note: The <HTTPClient> does not need to be a class. It is just a contract defining which external functionality the RemoteFeedLoader needs, so a protocol is a more suitable way to define it.
 //note: By creating a clean separation with protocols, we made the RemoteFeedLoader more flexible, open for extension and more testable.
 public protocol HTTPClient {
-    func get(from url:URL)
+    func get(from url:URL, completion: @escaping (Error) -> Void)
 }
 
 
@@ -18,15 +18,22 @@ final public class RemoteFeedLoader {
 
     private let url:URL
     private let client:HTTPClient
+    
+    public enum Error: Swift.Error {
+        case connectivity
+    }
 
     public init(url:URL, client: HTTPClient) {
         self.url = url
         self.client = client
     }
     
-    public func load() {
+    public func load(completion: @escaping (Error) -> Void = { _ in }) {
         //note: Move the test logic from RemoteFeedLoader to HTTPClient
-        client.get(from: url)
+        client.get(from: url) { error in
+            //map the HTTPClient error into domain specific error (e.g. connectivity error)
+            completion(.connectivity)
+        }
         //client.get(from: url) //note: mistakes like this happen all the time (especially when merging code) 
     }
 }
--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -81,6 +81,19 @@ final class RemoteFeedLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedURLs, [url, url])
     }
     
+    func test_load_deliversErrorOnClientError() {
+        let (sut, client) = makeSUT()
+        var capturedError:RemoteFeedLoader.Error?
+        client.error = NSError(domain: "Test", code: 0) //properties injection
+        
+        sut.load { error in
+            capturedError = error
+        }
+        
+        XCTAssertEqual(capturedError, .connectivity)
+    }
+    
+    
     // MARK: - Helpers
     
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
@@ -91,10 +104,14 @@ final class RemoteFeedLoaderTests: XCTestCase {
     class HTTPClientSpy: HTTPClient {
         
         var requestedURLs:[URL] = []
+        var error:Error?
         
-        func get(from url:URL) {
+        func get(from url: URL, completion: @escaping (Error) -> Void) {
+            
+            if let error = error {
+                completion(error)
+            }
             requestedURLs.append(url)
-            //requestedURL = url
         }
     }
 
